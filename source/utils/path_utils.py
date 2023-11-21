88ae9957ffbd7cc0ef743134c07e8beec06cca67
Cyrus Wong <cyruss081115@gmail.com>
2023-11-21 23:34:21 +0800

    First commit

---
+++ source/utils/path_utils.py
@@ -0,0 +1,5 @@
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+DATASETS_ROOT_DIR = os.path.join(PROJECT_DIR, 'datasets')
+SOURCE_DIR = os.path.join(PROJECT_DIR, 'source')
